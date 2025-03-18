c3e60f7488044d83501c16e5b4ddf25b4424d446
Rye M <ryem@Ryes-MBP.lan>
2025-03-18 10:57:17 -0400

    minor change

--- arduino_clueboard/src/main.cpp
+++ arduino_clueboard/src/main.cpp
@@ -1,147 +1,47 @@
-#include <Adafruit_VCNL4040.h>
+#include "Adafruit_Arcada.h"
+Adafruit_Arcada arcada;
 
-Adafruit_VCNL4040 vcnl4040 = Adafruit_VCNL4040();
-unsigned long Time_marker_previous = 0;
-unsigned long Time_marker = 0;
-bool proximityTimeStored = false;
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print("Hello! Arcada TFT Test");
 
-void setup() {
-  Serial.begin(115200);
-  // Wait until serial port is opened
-  while (!Serial) { delay(1); }
-
-  Serial.println("Adafruit VCNL4040 Config demo");
-
-  if (!vcnl4040.begin()) {
-    Serial.println("Couldn't find VCNL4040 chip");
-    while (1);
+  // Start TFT and fill black
+  if (!arcada.arcadaBegin()) {
+    Serial.print("Failed to begin");
+    while (1) delay(10);
   }
-  Serial.println("Found VCNL4040 chip");
-
-  //vcnl4040.setProximityLEDCurrent(VCNL4040_LED_CURRENT_200MA);
-  Serial.print("Proximity LED current set to: ");
-  switch(vcnl4040.getProximityLEDCurrent()) {
-    case VCNL4040_LED_CURRENT_50MA: Serial.println("50 mA"); break;
-    case VCNL4040_LED_CURRENT_75MA: Serial.println("75 mA"); break;
-    case VCNL4040_LED_CURRENT_100MA: Serial.println("100 mA"); break;
-    case VCNL4040_LED_CURRENT_120MA: Serial.println("120 mA"); break;
-    case VCNL4040_LED_CURRENT_140MA: Serial.println("140 mA"); break;
-    case VCNL4040_LED_CURRENT_160MA: Serial.println("160 mA"); break;
-    case VCNL4040_LED_CURRENT_180MA: Serial.println("180 mA"); break;
-    case VCNL4040_LED_CURRENT_200MA: Serial.println("200 mA"); break;
-  }
+  arcada.displayBegin();
   
-  //vcnl4040.setProximityLEDDutyCycle(VCNL4040_LED_DUTY_1_40);
-  Serial.print("Proximity LED duty cycle set to: ");
-  switch(vcnl4040.getProximityLEDDutyCycle()) {
-    case VCNL4040_LED_DUTY_1_40: Serial.println("1/40"); break;
-    case VCNL4040_LED_DUTY_1_80: Serial.println("1/80"); break;
-    case VCNL4040_LED_DUTY_1_160: Serial.println("1/160"); break;
-    case VCNL4040_LED_DUTY_1_320: Serial.println("1/320"); break;
-  }
-
-  //vcnl4040.setAmbientIntegrationTime(VCNL4040_AMBIENT_INTEGRATION_TIME_80MS);
-  Serial.print("Ambient light integration time set to: ");
-  switch(vcnl4040.getAmbientIntegrationTime()) {
-    case VCNL4040_AMBIENT_INTEGRATION_TIME_80MS: Serial.println("80 ms"); break;
-    case VCNL4040_AMBIENT_INTEGRATION_TIME_160MS: Serial.println("160 ms"); break;
-    case VCNL4040_AMBIENT_INTEGRATION_TIME_320MS: Serial.println("320 ms"); break;
-    case VCNL4040_AMBIENT_INTEGRATION_TIME_640MS: Serial.println("640 ms"); break;
-  }
-
-
-  //vcnl4040.setProximityIntegrationTime(VCNL4040_PROXIMITY_INTEGRATION_TIME_8T);
-  Serial.print("Proximity integration time set to: ");
-  switch(vcnl4040.getProximityIntegrationTime()) {
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_1T: Serial.println("1T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_1_5T: Serial.println("1.5T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_2T: Serial.println("2T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_2_5T: Serial.println("2.5T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_3T: Serial.println("3T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_3_5T: Serial.println("3.5T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_4T: Serial.println("4T"); break;
-    case VCNL4040_PROXIMITY_INTEGRATION_TIME_8T: Serial.println("8T"); break;
-  }
-
-  //vcnl4040.setProximityHighResolution(false);
-  Serial.print("Proximity measurement high resolution? ");
-  Serial.println(vcnl4040.getProximityHighResolution() ? "True" : "False");
-
-  Serial.println("");
-
+  // Turn on backlight
+  arcada.setBacklight(255); 
 }
 
 void loop() {
-  
-  if (vcnl4040.getProximity() >= 50){
-    Time_marker = millis(); 
+  arcada.display->fillScreen(ARCADA_RED);
+#if defined(ADAFRUIT_MONSTER_M4SK_EXPRESS)
+  arcada.display2->fillScreen(ARCADA_RED);
+#endif
 
-    Serial.print("Proximity: "); Serial.print(vcnl4040.getProximity()); Serial.print("  ");
-    Serial.print("Interval: ");  Serial.println(Time_marker - Time_marker_previous);
+  delay(100);
 
-    Time_marker_previous = Time_marker;
-  }
- 
-  delay(1);
+  arcada.display->fillScreen(ARCADA_GREEN);
+#if defined(ADAFRUIT_MONSTER_M4SK_EXPRESS)
+  arcada.display2->fillScreen(ARCADA_GREEN);
+#endif
+
+  delay(100);
+
+  arcada.display->fillScreen(ARCADA_BLUE);
+#if defined(ADAFRUIT_MONSTER_M4SK_EXPRESS)
+  arcada.display2->fillScreen(ARCADA_BLUE);
+#endif
+
+  delay(100);
+
+  arcada.display->fillScreen(ARCADA_BLACK);
+#if defined(ADAFRUIT_MONSTER_M4SK_EXPRESS)
+  arcada.display2->fillScreen(ARCADA_BLACK);
+#endif
+
+  delay(100);
 }
-
-
-
-
-
-/***************************************************************************
-  This is a library for the APDS9960 digital proximity, ambient light, RGB, and gesture sensor
-
-  This sketch puts the sensor in proximity mode and enables the interrupt
-  to fire when proximity goes over a set value
-
-  Designed specifically to work with the Adafruit APDS9960 breakout
-  ----> http://www.adafruit.com/products/3595
-
-  These sensors use I2C to communicate. The device's I2C address is 0x39
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Dean Miller for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
-// #include "Adafruit_APDS9960.h"
-
-// //the pin that the interrupt is attached to
-// #define INT_PIN 3
-
-// //create the APDS9960 object
-// Adafruit_APDS9960 apds;
-
-// void setup() {
-//   Serial.begin(115200);
-
-//   if(!apds.begin()){
-//     Serial.println("failed to initialize device! Please check your wiring.");
-//   }
-//   else Serial.println("Device initialized!");
-
-//   //enable proximity mode
-//   apds.enableProximity(true);
-
-//   //set the interrupt threshold to fire when proximity reading goes above 175
-//   apds.setProximityInterruptThreshold(0, 175);
-
-//   //enable the proximity interrupt
-//   apds.enableProximityInterrupt();
-// }
-
-// void loop() {
-
-//   //print the proximity reading when the interrupt pin goes low
-//   if(!digitalRead(INT_PIN)){
-//     Serial.println(apds.readProximity());
-
-//     //clear the interrupt
-//     apds.clearInterrupt();
-//   }
-// }
-// """
