fb03cc940e5eb86cbd8a13867efc006df3289635
Rye M <ryem@Ryes-MBP.lan>
2025-03-18 13:16:44 -0400

    minor change

--- arduino_clueboard/src/main.cpp
+++ arduino_clueboard/src/main.cpp
@@ -2,32 +2,35 @@
 
 Adafruit_Arcada arcada;
 
-#define NUM_SELECTIONS 3
-const char *selection[NUM_SELECTIONS] = {"Apples", "Bananas", "Pears"};
 
 void setup() {
-  //while (!Serial) delay(10);
-
-  Serial.println("Hello! Arcada Menu test");
+  Serial.begin(115200);
+ // while (!Serial);
+  
+  Serial.println("Hello! Arcada Alerts test");
   if (!arcada.arcadaBegin()) {
     Serial.print("Failed to begin");
-    while (1);
+    while (1) { delay(1); }
   }
   arcada.displayBegin();
   Serial.println("Arcada display begin");
   arcada.setBacklight(255);
+  
+  arcada.display->fillScreen(ARCADA_RED);
+  arcada.infoBox("Arcada alerts test! This is an info box with many lines of text?");
+
+  arcada.display->fillScreen(ARCADA_GREEN);
+  arcada.warnBox("Arcada alerts test! This is an warning box ... watch out!", ARCADA_BUTTONMASK_B);
+
+  arcada.display->fillScreen(ARCADA_BLUE);
+  arcada.errorBox("Arcada alerts test! This is an error box, something went wrong!",  ARCADA_BUTTONMASK_START);
+
+  arcada.display->fillScreen(ARCADA_BLACK);
+  arcada.haltBox("Arcada alerts test! This is an halting alert box :(");
 }
 
 
 void loop() {
-  arcada.display->fillScreen(ARCADA_RED);
-  uint8_t selected = arcada.menu(selection, NUM_SELECTIONS, ARCADA_WHITE, ARCADA_BLACK);
-
-  char message[80];
-  sprintf(message, "Selected '%s'", selection[selected]);
-  arcada.display->fillScreen(ARCADA_BLUE);
-  arcada.infoBox(message);
 }
-
 
 /*****************************************************************/
